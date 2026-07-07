a5142357d39716479dc6bb177ff9f3a373420219
Akshat <akshat0746@gmail.com>
2026-07-07 23:39:23 +0530

    function for toggle video like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,7 +6,35 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+     if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id")
+    }
+
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { isLiked: false }, "Video unliked successfully"))
+    }
+
+    const like = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    if (!like) {
+        throw new ApiError(500, "Something went wrong while liking the video")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }, "Video liked successfully"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
