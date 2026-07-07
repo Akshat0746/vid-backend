9f9024abafdb0eb196641289b2253a963c6d54f4
Akshat <akshat0746@gmail.com>
2026-07-07 23:43:24 +0530

    function for toggle subscription

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,9 +8,35 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    // TODO: toggle subscription
+      if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+    // Prevent a user from subscribing to their own channel
+    if (channelId === req.user?._id.toString()) {
+        throw new ApiError(400, "You cannot subscribe to your own channel")
+    }
+    const channel = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+    const existingSubscription = await Subscription.findOne({
+        subscriber: req.user?._id,
+        channel: channelId
+    })
+    if (existingSubscription) {
+        await Subscription.findByIdAndDelete(existingSubscription._id)
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { subscribed: false }, "Unsubscribed successfully"))
+    }
+    const newSubscription = await Subscription.create({
+        subscriber: req.user?._id,
+        channel: channelId
+    })
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { subscribed: true, subscription: newSubscription }, "Subscribed successfully"))
 })
-
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
