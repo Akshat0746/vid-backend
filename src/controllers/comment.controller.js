6c8fa6bd11c06fe42159a09a88683a51734cf04d
Akshat <akshat0746@gmail.com>
2026-07-24 22:58:30 +0530

    function to add comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -12,7 +12,29 @@ const getVideoComments = asyncHandler(async (req, res) => {
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {content} = req.body
+    if (!content?.trim()) {
+        throw new ApiError(400, "Comment content is required")
+    }
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user?._id
+    })
+    if (!comment) {
+        throw new ApiError(500, "Failed to add comment")
+    }
+    return res
+        .status(201)
+        .json(new ApiResponse(201, comment, "Comment added successfully"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
